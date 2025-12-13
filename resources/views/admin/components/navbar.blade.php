332386368e17d532b9c431a9fe62745d185ea473
russ <russelcuevas0@gmail.com>
2025-12-13 15:42:00 +0800

    first

---
+++ resources/views/admin/components/navbar.blade.php
@@ -0,0 +1,116 @@
+<div class="offcanvas offcanvas-start" tabindex="-1" id="sidebarOffcanvas" aria-labelledby="sidebarOffcanvasLabel">
+        <div class="offcanvas-header pb-0">
+            <h4 class="px-4" id="sidebarOffcanvasLabel" style="color:#0d6efd; font-weight:700;">
+                <span class="nav-icon me-2">
+                    <i class="fa-solid fa-cloud-rain"></i> 
+                </span>
+                MDRRMO
+            </h4>
+            <button type="button" class="btn-close text-reset me-2" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <ul class="nav flex-column mb-auto"> 
+                <li class="nav-item">
+                    <a class="nav-link nav-link-custom active" href="#">
+                        <span class="nav-icon"><i class="fa-solid fa-table-cells-large"></i></span>
+                        Dashboard
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-custom" href="#">
+                        <span class="nav-icon"><i class="fa-solid fa-lightbulb"></i></span>
+                        AI Forecast
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-custom" href="#">
+                        <span class="nav-icon"><i class="fa-solid fa-water"></i></span>
+                        Flood Risk
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-custom" href="#">
+                        <span class="nav-icon"><i class="fa-solid fa-wave-square"></i></span>
+                        Earthquakes
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link nav-link-custom" href="#">
+                        <span class="nav-icon"><i class="fa-solid fa-gear"></i></span>
+                        Settings
+                    </a>
+                </li>
+            </ul>
+            </div>
+    </div>
+
+
+    <div class="sidebar-desktop">
+        <h4 class="px-4 mb-4" style="color:#0d6efd; font-weight:700;">
+            <span class="nav-icon me-2">
+                <i class="fa-solid fa-cloud-rain"></i> 
+            </span>
+            MDRRMO
+        </h4>
+
+        <ul class="nav flex-column">
+            <li class="nav-item">
+                <a class="nav-link nav-link-custom active" href="#">
+                    <span class="nav-icon"><i class="fa-solid fa-table-cells-large"></i></span>
+                    Dashboard
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-link-custom" href="#">
+                    <span class="nav-icon"><i class="fa-solid fa-lightbulb"></i></span>
+                    AI Forecast
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-link-custom" href="#">
+                    <span class="nav-icon"><i class="fa-solid fa-water"></i></span>
+                    Flood Risk
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-link-custom" href="#">
+                    <span class="nav-icon"><i class="fa-solid fa-wave-square"></i></span>
+                    Earthquakes
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link nav-link-custom" href="#">
+                    <span class="nav-icon"><i class="fa-solid fa-gear"></i></span>
+                    Settings
+                </a>
+            </li>
+        </ul>
+        </div>
+
+    <nav class="top-navbar sticky-top p-3">
+        <div class="d-flex justify-content-between align-items-center">
+
+            <button class="navbar-toggler d-lg-none p-0 border-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebarOffcanvas" aria-controls="sidebarOffcanvas" aria-label="Toggle navigation">
+                <i class="fa-solid fa-bars fa-lg"></i>
+            </button>
+
+            <h5 class="mb-0 text-muted d-none d-lg-block"></h5>
+
+            <div class="dropdown">
+                <button class="btn btn-outline-primary" type="button" data-bs-toggle="dropdown" aria-expanded="false" title="User Profile">
+                    <i class="fa-regular fa-user profile-dropdown-icon"></i>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end">
+                    <li><h6 class="dropdown-header">Welcome, User!</h6></li>
+                    <li><a class="dropdown-item" href="#">
+                        <i class="fa-solid fa-id-badge me-2"></i> Profile
+                    </a></li>
+                    <li><hr class="dropdown-divider"></li>
+                    <li><a class="dropdown-item text-danger" href="#">
+                        <i class="fa-solid fa-sign-out-alt me-2"></i> Logout
+                    </a></li>
+                </ul>
+            </div>
+
+        </div>
+    </nav>
